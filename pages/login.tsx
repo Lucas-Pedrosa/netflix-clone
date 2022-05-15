289a540c9b1a0f33f3d42bfd0b95d9ad32c5610e
lucas-pedrosa <lucas.dias.pedrosa@gmail.com>
2022-05-15 15:31:27 -0300

    Created useAuth hook

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,6 +3,7 @@ import Head from "next/head"
 import Image from "next/image"
 import { useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import useAuth from "../hooks/useAuth"
 
 interface Inputs {
   email: string
@@ -11,6 +12,8 @@ interface Inputs {
 
 const login: NextPage = () => {
   const [login, setLogin] = useState(false)
+  const { signIn, signUp } = useAuth()
+
   const {
     register,
     handleSubmit,
@@ -18,9 +21,9 @@ const login: NextPage = () => {
   } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
     if (login) {
-      //await signIn(email, password)
+      await signIn(email, password)
     } else {
-      //await signUp(email, password)
+      await signUp(email, password)
     }
   }
 
