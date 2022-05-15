1436568c820743af892a977b348c5a2d70f3a218
lucas-pedrosa <lucas.dias.pedrosa@gmail.com>
2022-05-15 17:44:00 -0300

    Finished movie modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 import Row from '../components/Row'
 import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
@@ -30,6 +31,7 @@ const Home = ({
   trendingNow
 }: Props) => {
   const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
 
   if (loading) {
     return null
@@ -57,7 +59,7 @@ const Home = ({
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
